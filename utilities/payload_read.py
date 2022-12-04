0b2fef4eed5146bd9802ce7588ff8a6e1beaac86
sofurs <sofurs@users.noreply.github.com>
2022-12-04 22:35:48 +0100

    update of README and utility scripts

--- utilities/payload_read.py
+++ utilities/payload_read.py
@@ -3,23 +3,24 @@
 import sys
 
 def help():
-    print("Usage: ./{} {{memory address}} {{offset}}".format(sys.argv[0]))
-    print("Address and value format: 0xXXXXXXXX")
+    print("Usage: {} {{memory address}} {{format character}} {{offset}}".format(sys.argv[0]))
+    print("Address format: 0xXXXXXXXX")
+    print("Format character: %s/%c/%x/%d/...")
     print("Offset - number of octets from $esp to start of buffer")
     exit(1)
 
 def get_address(address):
     return address.to_bytes(4, 'little')
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     help()
 
 address = int(sys.argv[1], 16)
-offset = int(sys.argv[2])
+format_char = sys.argv[2]
+offset = int(sys.argv[3])
 
 sys.stdout.buffer.write(get_address(address))
 
-output = ''.join(['%x' for i in range(offset)])
-output += 'Data: %s'
+output = '%' + str(offset + 1) + '$' + format_char[1:]
 
 print(output, end='')
